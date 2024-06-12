0be24f1d2cdbbe73ed51eb3f8a93dc098fe4fcfb
Ubuntu <ubuntu@ip-172-31-2-206.ap-south-1.compute.internal>
2024-06-12 12:42:15 +0000

    added stage files

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World from Node.js on Kubernetes for Dev env!');
+  res.send('Hello World from Node.js on Kubernetes for stage env!');
 });
 
 app.listen(port, () => {
